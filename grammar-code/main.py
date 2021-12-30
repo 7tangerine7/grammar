5053422b9caeab05877967661dd3aa4c23db5f25
Polina <ptukhap@gmail.com>
2021-12-31 00:25:51 +0200

    it's working

--- grammar-code/main.py
+++ grammar-code/main.py
@@ -8,31 +8,31 @@ def f(sample, state, d, row_s, row_e, col_s, col_e):
         return q(sample, standard_zero)
     elif state == 1:
         return q(sample, standard_one)
-    else:
+    elif not np.any(d[state, row_s, row_e, col_s, col_e]):
         h_check = False
-        for iii in range(row_s+1, row_e):
-            for kU in range(state+1):
-                for kD in range(state + 1):
+        for iii in range(row_s, row_e):
+            for kU in range(state):
+                for kD in range(state):
                     if (kU, kD) in vertical:
-                        h_check = h_check or (d[kD, row_s, iii, col_s, col_e] and d[kU, iii+1, row_e, col_s, col_e]
-                                              and vertical[(kU, kD)] == state)
+                        h_check = h_check or (d[kU, row_s, iii, col_s, col_e] and d[kD, iii+1, row_e, col_s, col_e] and vertical[(kU, kD)] == state)
         w_check = False
-        for jjj in range(col_s+1, col_e):
-            for kL in range(state+1):
-                for kR in range(state+1):
+        for jjj in range(col_s, col_e):
+            for kL in range(12):
+                for kR in range(12):
                     if (kL, kR) in horizontal:
                         w_check = w_check or (d[kL, row_s, row_e, col_s, jjj] and d[kR, row_s, row_e, jjj+1, col_e] and
                                               horizontal[(kL, kR)] == state)
         r_check = False
-        for kkk in range(2):
+        for kkk in range(6, 12):
             if kkk in rename:
-                r_check = r_check or (d[kkk, row_s, row_e, col_s, col_e] and rename[kkk])
-    return h_check or w_check or r_check
+                r_check = r_check or (d[kkk, row_s, row_e, col_s, col_e] and rename[kkk] == state)
+        return h_check or w_check or r_check
+    return False
 
 
 def q(sample, standard):
     if np.shape(sample) == np.shape(standard):
-        if np.equal(sample, standard):
+        if np.array_equal(sample, standard):
             return True
     else:
         return False
@@ -41,20 +41,17 @@ def q(sample, standard):
 def CYK(test):
     H, W = np.shape(test)
     num_col = W // w
-    d = np.empty((12, 3, 3, num_col, num_col))
-    it_v_s, it_v_e, it_g_s, it_g_e = 0, 0, 0, 0
+    d = np.zeros((12, 3, 3, num_col, num_col), dtype=bool)
     for s in range(h*w, H*W+1, h*w):
         for i in range(0, H, h):
-            it_v_s += 1
+            if s > 1419 and i == 2*h: continue
             for j in range(0, W, w):
-                it_v_e += 1
-                for i_end in range(i + h, H + h, h):
-                    it_g_s += 1
-                    for j_end in range(j + w, W + w, w):
-                        it_g_e += 1
+                for i_end in range(i+h, H+1, h):
+                    for j_end in range(j+w, W+w, w):
                         if (i_end - i)*(j_end - j) != s:
                             continue
                         else:
+                            sample_ = test[i:i_end, j:j_end]
                             for k in range(12):
                                 if i != 0:
                                     row_s_ = i//h
@@ -66,24 +63,23 @@ def CYK(test):
                                 else:
                                     col_s_ = 0
                                 col_e_ = j_end//w - 1
-                                sample_ = test[i:i_end+1][j:j_end+1]
                                 d[k, row_s_, row_e_, col_s_, col_e_] = f(sample_, k, d, row_s_, row_e_, col_s_, col_e_)
     if np.any(d[:, 0, 2, 0, num_col-1]) == 1:
-        return True
+        print("Correct!")
+        if d[11, 0, 2, 0, 0]:
+            print("Без переносу")
+        if d[10, 0, 2, 0, 0]:
+            print("З переносом")
     else:
-        return False
+        print("Incorrect")
 
 
 def main():
-    v1 = np.concatenate((standard_zero, standard_zero, standard_one), axis=0)
-    v2 = np.concatenate((standard_zero, standard_zero, standard_one), axis=0)
-    v3 = np.concatenate((standard_zero, standard_one, standard_zero), axis=0)
-    test = np.concatenate((v1, v2, v3), axis=1)
-    answer = CYK(test)
-    if answer is True:
-        print("Correct!")
-    else:
-        print("Incorrect")
+    v1 = np.concatenate((standard_zero, standard_zero, standard_zero), axis=1)
+    v2 = np.concatenate((standard_zero, standard_zero, standard_one), axis=1)
+    v3 = np.concatenate((standard_zero, standard_zero, standard_one), axis=1)
+    test = np.concatenate((v1, v2, v3), axis=0)
+    CYK(test)
 
 
 if __name__ == '__main__':
@@ -98,13 +94,19 @@ if __name__ == '__main__':
     h, w = np.shape(standard_zero)
     horizontal = {
         (10, 6): 10,
-        (11, 9): 11,
         (10, 7): 11,
-        (11, 8): 11
+        (11, 7): 10,
+        (11, 6): 10,
+        (10, 8): 10,
+        (11, 7): 11,
+        (11, 9): 11,
+        (10, 9): 11
     }
     rename = {
-        11: 9,
-        10: 8
+        8: 11,
+        9: 11,
+        7: 10,
+        6: 10
     }
     vertical = {
         (1, 1): 2,
